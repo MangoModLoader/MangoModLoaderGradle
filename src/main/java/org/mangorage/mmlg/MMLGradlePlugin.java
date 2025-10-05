d3ba15b605d81a898444d7eacae8c69335d13ec6
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2025-10-05 06:06:56 -0700

    Initial 1.0.0

---
+++ src/main/java/org/mangorage/mmlg/MMLGradlePlugin.java
@@ -0,0 +1,22 @@
+package org.mangorage.mmlg;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.mangorage.mmlg.tasks.RunGame;
+
+public final class MMLGradlePlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+        project.getConfigurations().create("gameJar", t -> {
+            t.setVisible(true);
+            t.setTransitive(false);
+        });
+
+        project.getConfigurations().create("bootJar", t -> {
+            t.setVisible(true);
+            t.setTransitive(false);
+        });
+
+        project.getTasks().register("runMCGame", RunGame.class, "minecraft");
+    }
+}
---
+++ src/main/java/org/mangorage/mmlg/tasks/RunGame.java
@@ -0,0 +1,64 @@
+package org.mangorage.mmlg.tasks;
+
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.tasks.JavaExec;
+import org.gradle.api.tasks.TaskAction;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+
+public class RunGame extends JavaExec {
+
+
+    public static Path getAll(Path input) {
+        String filename = input.getFileName().toString();
+        int dotIndex = filename.lastIndexOf('.');
+        String newFilename = (dotIndex != -1)
+                ? filename.substring(0, dotIndex) + "-all" + filename.substring(dotIndex)
+                : filename + "-all";
+
+        return input.getParent() != null
+                ? input.getParent().resolve(newFilename)
+                : Path.of(newFilename);
+    }
+
+    public static Path copyFileTo(Path source, Path dest) throws IOException {
+        // Ensure parent directory exists
+        System.out.println("Copied " + source + " -> " + dest);
+
+        Files.createDirectories(dest.getParent());
+
+        // Copy file (overwrite if exists)
+        return Files.copy(source, dest, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    @Inject
+    public RunGame(String group) {
+        setGroup(group);
+
+        Path bootJar = getAll(getProject().getConfigurations().getByName("bootJar").getSingleFile().toPath());
+        Path mcJar = getProject().getConfigurations().getByName("gameJar").getSingleFile().toPath();
+
+        try {
+            copyFileTo(mcJar, getProject().getProjectDir().toPath().resolve("build").resolve("run").resolve("classpath-game").resolve("minecraft.jar"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(getProject().getProjectDir());
+
+        setWorkingDir(getProject().file("build/run/"));
+        setClasspath(getProject().files(bootJar));
+        getMainClass().set("org.mangorage.boot.Boot");
+    }
+
+    @Override
+    public List<String> getArgs() {
+        return List.of("--minecraftJar", "jarHere!");
+    }
+
+}
